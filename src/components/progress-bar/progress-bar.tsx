7f35eb0f688e1d4829c83ad9e622cf919bacc9b7
Briam <briammartinez@gmail.com>
2019-12-01 01:18:00 +0100

    feat: Created w-progress-bar component

---
+++ src/components/progress-bar/progress-bar.tsx
@@ -0,0 +1,21 @@
+import { Component, Host, h } from '@stencil/core';
+
+@Component({
+  tag: 'w-progress-bar',
+  styleUrl: 'progress-bar.css',
+  shadow: true
+})
+export class ProgressBar {
+
+  render() {
+    return (
+      <Host>
+        <div class="progress-bar">
+          <div class="progress-item progress-bar-content"><slot name="intial-value"/></div>
+          <div class="progress-item progress-final-value"><slot name="final-value"/></div>
+        </div>
+      </Host>
+    );
+  }
+
+}
